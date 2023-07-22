bf984944d91f1fae6a49a9d241f22b4b5f78dd1d
Le Minh Hanh <hanhlmph26629@fpt.edu.vn>
2023-07-22 23:02:35 +0700

    Entity

---
+++ entity/HoaDonCT.java
@@ -0,0 +1,33 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "HoaDonCT", schema = "dbo")
+public class HoaDonCT {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "Id", nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "IdHoaDon", referencedColumnName = "Id")
+    private HoaDon idHoaDon;
+
+    @Size(max = 20)
+    @Column(name = "SoLuong", length = 20)
+    private String soLuong;
+
+    @Column(name = "DonGia", precision = 20)
+    private BigDecimal donGia;
+
+}
