9a6268863a3dbb7b21c3686c3ef41cd095ac30db
tuphph24187 <tuphph24187@fpt.edu.vn>
2023-08-04 15:01:00 +0700

    no message

--- src/test/java/com/example/da_quanlybangiay/DaQuanLyBanGiayApplicationTests.java
+++ src/test/java/com/example/da_quanlybangiay/DaQuanLyBanGiayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DaQuanLyBanGiayApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
